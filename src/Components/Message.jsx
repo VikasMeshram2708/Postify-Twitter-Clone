3b52517f5b0730198fe9582d2a70651485540d96
VikasMeshram2708 <meshramvikas744@gmail.com>
2023-02-13 11:03:15 +0530

    :sparkles: completed message component

--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -3,48 +3,26 @@ import React from 'react';
 import '../Styles/Message.css';
 
 const Message = () => {
-  return <div className='message'>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-    <h3>Messages</h3>
-  </div>;
+  return (
+    <div className="message">
+      <div className="message__userDetails">
+        {/* user img */}
+        <img src="https://is.gd/5MnRqq" className="message__userImg" alt="" />
+        {/* text field */}
+        <input
+          type="text"
+          placeholder="What's happening ?"
+          className="message__userText"
+        />
+      </div>
+      {/* Tweet button */}
+      <div className="message__button">
+        <button type="button" className="message__tweetBtn">
+          Tweet
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Message;
